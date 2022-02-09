1ce4300019badd887d4b4c8cd3cbdd3adf917709
TheMrChaptastic <chappy@themrchaptastic.com>
2022-02-09 10:58:26 -0600

    Add project files.

---
+++ BonusAPIExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BonusAPIExercise
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+        }
+    }
+}
